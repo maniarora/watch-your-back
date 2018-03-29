6c4074717f242cd0f47316964554f8f3baf3a74e
Manindra <manindraa@student.unimelb.edu.au>
2018-03-29 14:18:05 +1100

    Heuristic function h

--- BoardProblem.py
+++ BoardProblem.py
@@ -1,19 +1,29 @@
 from board import Board
-from search import Problem
+import aima
+from search import *
+from utils import *
+
+class Problem(object):
+    def __init__(self, initial, goal=None):
+        """The constructor specifies the initial state, and possibly a goal
+        state, if there is a unique goal. Your subclass's constructor can add
+        other arguments."""
+        self.initial = initial
+        self.goal = goal
 
 class BoardProblem(Problem):
-    
-    def __init__(self,initial,goal=None):
-        super().__init__(initial, goal)
-        
-    
+
+    def __init__(self, initial, goal):
+        super().__init__(self,initial,goal)
+        self.initial = initial
+
     def result(self, state,action):
-        
+
         player = "O"
         opponent = "@"
         empty = '-'
         blocked = 'X'
-        
+
 
         row = action[1][0]
         col = action[1][1]
@@ -23,11 +33,11 @@ class BoardProblem(Problem):
             if col > 0 and state.board[row][col-1] != blocked:
                 if col-1 == empty:
                     return state.makeMove([row,col],[row,col-1])
-                        
+
                 elif col-1 == opponent or state.board[row][col-1] == player:
                     if col > 1 and state.board[row][col-2] == empty:
                         return state.makeMove([row,col],[row,col-2])
-            
+
         #Right Movement
         if action[0] == "RIGHT":
             if col < Board.WIDTH-1 and state.board[row][col+1] != blocked:
@@ -35,7 +45,7 @@ class BoardProblem(Problem):
                     return state.makeMove([row,col],[row,col+1])
                 elif state.board[row][col+1] == opponent or state.board[row][col+1] == player:
                     if col < Board.WIDTH-2 and state.board[row][col+2] == empty:
-                            return state.makeMove([row,col],[row,col+2])
+                        return state.makeMove([row,col],[row,col+2])
 
         #Up Movement
         if action[0] == "UP":
@@ -50,37 +60,42 @@ class BoardProblem(Problem):
         if action[0] == "DOWN":
             if row < Board.HEIGHT-1 and state.board[row+1][col] != blocked:
                 if state.board[row+1][col] == empty:
-                    return state.makeMove([row,col],[row+1,col])
-
+                    return state.makeMove([row,col], [row + 1, col])
                 elif state.board[row+1][col] == opponent or state.board[row+1][col] == player:
                     if row < Board.HEIGHT-2 and state.board[row+2][col] == empty:
                         return state.makeMove([row,col],[row+2,col])
-                            
-    
-    
+
+
+
     def actions(self,state):
-        
+
         whites =  state.getLoc("white")
-        
+
+
         actions = []
         for i in whites:
             actions.append(["UP",i])
             actions.append(["DOWN",i])
             actions.append(["LEFT",i])
             actions.append(["RIGHT",i])
-            
+
         return actions
-            
+
     def goal_test(self,state):
         if len(state.getLoc("black")) == 0 :
             return True
         else:
-           return False
+            return False
+
+
+
+    def h(self, node):
+        x1, y1 = node.state.getLoc()
+        x2, y2 = self.goal
+        return abs(x2-x1) + abs(y2-y1)
 
     def heuristic(a, b):
         (x1, y1) = a
         (x2, y2) = b
         return abs(x2 - x1) + abs(y2 - y1)
 
-    
-            
