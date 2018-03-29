1849bbb67ef805d4cd433f1f883abdb7c4e86167
Manindra <manindraa@student.unimelb.edu.au>
2018-03-29 23:02:24 +1100

    new and improved

--- board.py
+++ board.py
@@ -3,10 +3,12 @@ import copy
 class Board:
     WIDTH = 8
     HEIGHT = 8
+
     def __init__(self):
         self.board = [[0 for x in range(Board.WIDTH)] for y in range(Board.HEIGHT)]
         self.whiteCount = 0
         self.blackCount  = 0
+        self.moveHistory = []
 
     # Reading in the board
     def populateBoardFromInput(self):
@@ -15,73 +17,78 @@ class Board:
             self.whiteCount += self.board[i].count('O')
             self.blackCount += self.board[i].count('@')
 
-    def __str__(self):
-        result = []
-        for i in range(Board.WIDTH):
-            result.append(self.board[i])
-        return str(result)
-
-    # Print board in order
-    def print_board(self):
-        for i in range(8):
-            str = ""
-            for j in range(8):
-                str += self.board[i][j]
-                str += " "
-            print(str)
-
-    def __call__(self,node):
-        return self.board
-    # Gets all x, y locations for the white player in the board
-    def getLoc(self, color):
-
-        result = []
-        goalState = [[0 for x in range(Board.WIDTH)] for y in range(Board.HEIGHT)]
-
-        if(color == 'white'):
-            player = 'O'
-        elif(color == 'black'):
-            player = '@'
-        
-        for i in range (Board.HEIGHT):
-            for j in range (Board.WIDTH):
-                goalState[i][j] = self.board[i][j]
-                if(self.board[i][j] == player):
-                    result.append([i,j])            # Change to [j,i] later for x, y coordinate system
-
-
-        return result
-    
     def makeMove(self, oldCoord, newCoord):
-        
-        result = copy.deepcopy(self)
 
-        result.board[oldCoord[0]][oldCoord[1]] = "-"
-        result.board[newCoord[0]][newCoord[1]] = "O"
+        # result = copy.deepcopy(self)
 
+        self.board[oldCoord[0]][oldCoord[1]] = "-"
+        self.board[newCoord[0]][newCoord[1]] = "O"
+
+        self.moveHistory.append((oldCoord),(newCoord))
         # print(print_board(self.board))
-        return result
-    
-    def isKilled(self, coord,color):
-        
+        # return result
+
+    def isKilled(self, coord, color):
+        # Checks if a piece is sandwitched, if yes, its killed directly
+
         empty = '-'
         blocked = 'X'
-        
+
         if color == 'black':
             player = '@'
             opponent = 'O'
         elif color == 'white':
             player = 'O'
             opponent = '@'
-            
-            
+
         row = coord[0]
         col = coord[1]
-        hostile = opponent+blocked
-        
-        if row > 0 and row <Board.WIDTH-1 and self.board[row+1][col] in hostile and self.board[row-1][col] in hostile:
-            self.board[row][col] = empty
+        hostile = opponent + blocked
 
-        if  col >0 and col < Board.WIDTH-1 and self.board[row][col+1] in hostile and self.board[row][col-1] in hostile:
-            self.board[row][col] = empty
-        
+        if row > 0 and row < Board.WIDTH - 1:
+            if self.board[row + 1][col] in hostile and self.board[row - 1][col] in hostile:
+                self.board[row][col] = empty
+
+        if col > 0 and col < Board.WIDTH - 1:
+            if self.board[row][col + 1] in hostile and self.board[row][col - 1] in hostile:
+                self.board[row][col] = empty
+
+
+
+
+
+
+
+# Helper functions
+def __str__(self):
+    result = []
+    for i in range(Board.WIDTH):
+        result.append(self.board[i])
+    return str(result)
+
+# Print board in order
+def print_board(self):
+    for i in range(8):
+        str = ""
+        for j in range(8):
+            str += self.board[i][j]
+            str += " "
+        print(str)
+
+def getLoc(self, color):
+    result = []
+
+    if (color == 'white'):
+        player = 'O'
+    elif (color == 'black'):
+        player = '@'
+
+    for i in range(Board.HEIGHT):
+        for j in range(Board.WIDTH):
+            if (self.board[i][j] == player):
+                result.append([i, j])  # Change to [j,i] later for x, y coordinate system
+
+    print(result)
+    return result
+
+
